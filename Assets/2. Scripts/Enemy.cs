8d68fe41d8adfa6d7a69e4947c53596b53ab0877
Hrpm <rjsdlek33@gmail.com>
2025-12-16 01:06:55 +0900

    [Fix] 타워 범위 개선
    - 타워에서 발사체를 발사 할 때 Range를 통해 정확히 발사 가능

--- Assets/2. Scripts/Enemy.cs
+++ Assets/2. Scripts/Enemy.cs
@@ -5,13 +5,41 @@ public class Enemy : MonoBehaviour
     // 적의 체력
     public float Health = 100f;
 
+    private TowerTargetDetector detector;
+
+    void Start()
+    {
+        detector = FindFirstObjectByType<TowerTargetDetector>();
+
+        //Debug.Log("[적] 생성됨");
+
+        if (detector != null)
+        {
+            detector.RegisterEnemy(this);
+            //Debug.Log("[적] 타워 타겟 감지기에 등록됨");
+        }
+        else
+        {
+            //Debug.LogError("[적] 타워 타겟 감지기를 찾을 수 없음!");
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        if (detector != null)
+        {
+            detector.UnregisterEnemy(this);
+            //Debug.Log("[적] 제거됨 → 감지기에서 등록 해제");
+        }
+    }
+
     // 타워 투사체 등으로부터 데미지를 받을 때 호출됨
     public void TakeDamage(float damage)
     {
         Health -= damage;
 
-        // 체력이 0 이하가 되면 제거
-        if (Health <= 0)
+        if (Health <= 0f)
         {
             Destroy(gameObject);
         }
