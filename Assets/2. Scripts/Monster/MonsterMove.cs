cb7a898d2bf2db0e99ccec23853715d96d922dd7
CHAE JONG AN <59220682+ChaeJongAn@users.noreply.github.com>
2025-12-19 09:04:27 +0900

    close #15 [프로토타입][몬스터] 몬스터 추가 애니메이션 적용
    - 불필요한 테스트맵 씬 삭제
    - 스폰 애니메이션이 작동 안하는 버그 진짜 해결
    애니메이션 프레임들에 좌표값이 있어서 자체적으로 좌표이동하고 리지드바디와 충돌했었음 (리지드바디 사용하면 자체제작 애니메이션이 사용안됨, 어플라이를 체크해야 애니메이션이 좌표값 위치로 안 튐)
    - 회전할때 애니가 어색한 버그 해결
    회전->이동 나가는 트랜지션 세팅값 문제 애니메이션 전환 위치 차이였음
    뚝뚝 끊기지 않고 애니 간 서로 자연스럽게 만들려면 세부조정 필요할듯

--- Assets/2. Scripts/Monster/MonsterMove.cs
+++ Assets/2. Scripts/Monster/MonsterMove.cs
@@ -7,17 +7,48 @@ public class MonsterMove : MonoBehaviour
 
     [Header("타겟 설정")]
     public Transform TargetAnchor; // 베이스캠프 도착지 타겟 위치 (인스펙터에서 설정 가능)
-
     public float AttackRange = 1f; // 공격 범위 (DB에 없어서 여기서 선언함)
+
+    // 방향 관련 
+    Vector3 nextDir; // 회전 후 바라볼 방향 
+    Vector3 currentLookDir; // 현재 바라보는 방향
+
+    // 회전 관련 
+    bool isTurning = false;
+    float turnProgress = 0f; // 회전 진행도 (0~1)
+    float turnDuration = 0.5f; // 회전에 걸리는 시간
+
+    
+    /// <summary>
+    /// 테스트용 웨이포인트 배열 나중에 주석처리 
+    /// </summary>
+    public Transform[] Waypoints; // 테스트용 웨이포인트 
+    int currentWaypointIndex = 0; // 현재 웨이포인트 인덱스
+    /// <summary>
+    /// 테스트용 웨이포인트 나중에 주석처리
+    /// </summary>
+    /// 
 
     void Start()
     {
         monster = GetComponent<Monster>();    // 몬스터 스크립트 가져오기
         animator = GetComponent<Animator>();  // 애니메이터 컴포넌트 가져오기
                                               // animator = transform.GetChild(0).GetComponent<Animator>(); // 몬스터 모델이 자식에 있을 때
+
+        currentLookDir = transform.forward; // 초기 방향 설정
+        
+        if(Waypoints != null && Waypoints.Length > 0)
+        {
+            currentLookDir = (Waypoints[0].position - transform.position).normalized;
+        }
+      
         Spawn();  // 스폰 될때 스폰 애니 재생 
     }
 
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(currentLookDir); // 매 프레임 마지막에 현재 바라보는 방향으로 회전 적용 
+    }
     void Update()
     {
         // 테스트용 
@@ -28,27 +59,89 @@ public class MonsterMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha5)) { TurnRight(); }
         if (Input.GetKeyDown(KeyCode.Alpha6)) { Spawn(); }
 
+        // 회전 처리
+        if (isTurning)
+        {
+            turnProgress += Time.deltaTime / turnDuration;
+   
+            if (turnProgress >= 1f)
+            {
+                isTurning = false;
+                currentLookDir = nextDir;
+            }
+        }
+
+        /// 테스트용 웨이포인트 
+        if (Waypoints != null && Waypoints.Length > 0)
+        {
+            if (currentWaypointIndex >= Waypoints.Length) return;
+            TargetAnchor = Waypoints[currentWaypointIndex];
+            float dist = Vector3.Distance(transform.position, TargetAnchor.position);
+            if (dist < 0.1f)
+            {
+                currentWaypointIndex++;
+
+                if (currentWaypointIndex >= Waypoints.Length)
+                {
+                    currentWaypointIndex = 0; // 무한 반복 
+                }
+                Vector3 nextWaypointDir = (Waypoints[currentWaypointIndex].position - transform.position).normalized;
+                SetDirection(nextWaypointDir);  // 길찾기가 호출할 메서드
+            }
+        }
+        /// 나중에 삭제 
+
+
         // 따라갈 타겟이 없으면 종료
-        if (TargetAnchor == null) return; 
+        if (TargetAnchor == null) return;
 
         float distance = Vector3.Distance(transform.position, TargetAnchor.position); // 타겟과의 거리 계산
 
-        if (distance > AttackRange) // 공격 범위 밖일 때 
+        if (distance > AttackRange)
         {
-            Vector3 direction = (TargetAnchor.position - transform.position).normalized; // 방향 벡터 계산
-
-            if (direction != Vector3.zero) // 방향이 0이 아닐 때만 회전  
+            // 이동 방향은 타겟을 향해
+            Vector3 moveDir = (TargetAnchor.position - transform.position).normalized;
+            transform.position += moveDir * monster.Data.MoveSpeed * Time.deltaTime;
+           
+            if (!isTurning)   // 회전 중이 아닐 때만 이동 방향을 바라봄
             {
-                transform.rotation = Quaternion.LookRotation(direction); // 몬스터가 이동방향으로 바라보게 함 방향 회전
+                currentLookDir = moveDir;
             }
-
-            transform.position = Vector3.MoveTowards(transform.position, TargetAnchor.position, monster.Data.MoveSpeed * Time.deltaTime); // 이동
-
         }
         else // 도착하고 공격 범위 안일때 공격
         {
             Attack();
         }
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        if (isTurning) return; // 이미 회전 중이면 무시
+
+        float turnAngle = Vector3.SignedAngle(currentLookDir, direction, Vector3.up);
+        if(turnAngle > 45f) // 방향 전환
+        {
+            TurnRight();
+            StartTurn(direction);
+        }
+        else if (turnAngle < -45f)  
+        {
+            TurnLeft();
+            StartTurn(direction);
+        }
+        else  // 같은 방향이면 앞 바라보게 설정
+        {     
+            currentLookDir = direction;
+        }
+    }
+    
+    // 회전 시작 메서드
+    void StartTurn(Vector3 targetDirection)
+    {
+        isTurning = true;
+
+        turnProgress = 0f; // 회전 진행도 초기화
+        nextDir = targetDirection; // 회전 후 바라볼 목표 방향 설정
     }
 
     // 스폰할 때 타겟 설정 (SpawnManager에서 호출)
@@ -60,7 +153,7 @@ public class MonsterMove : MonoBehaviour
     // 테스트용 총알 충돌 (총알 프리팹에 태그 불렛과, 콜라이더에 Is Trigger 체크 필요)
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Bullet")) // 총알 태그와 충돌했을 때 // 
+        if (other.CompareTag("Bullet")) // 총알 태그와 충돌했을 때 // 
         {
             OnHit(); // 피격 애니메이션 호출 
            // Destroy(other.gameObject); // 총알 파괴
@@ -68,8 +161,8 @@ public class MonsterMove : MonoBehaviour
 
         // 나중에 hp도 까이게 
     }
-   
-    public void OnHit() 
+
+    public void OnHit()
     {
         animator.SetTrigger("Hit");   // 피격 애니메이션 다 출력되면 다시 이동으로 바뀜. 다른 메서드도 동일 
 
@@ -79,7 +172,7 @@ public class MonsterMove : MonoBehaviour
     void Attack()
     {
         animator.SetTrigger("Attack");
-        
+
         // 아마 여기서 베이스캠프를 공격하면 피해를 입히게 할 듯
     }
     void Dead()
@@ -89,13 +182,10 @@ public class MonsterMove : MonoBehaviour
         // 죽으면 이동도 안되게 해야 함 
     }
 
-    // 길찾기에서 호출할 회전 메서드 
-    void TurnLeft() 
+
+    void TurnLeft()
     {
         animator.SetTrigger("TurnLeft");
-
-        // 적이 앞만 바라보고 가는 구조라 회전을 여기서 확인할 수가 없음 
-        // 그냥 길찾기에서 회전이 필요할 때 이 메서드를 호출하게 하는 게 편할듯 
     }
 
     void TurnRight()
