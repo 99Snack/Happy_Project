c4a880f4fab7fcabc3e92ffe2173813ec2a10bf8
CommonMango <73099231+CommonMango@users.noreply.github.com>
2025-12-20 23:51:27 +0900

    [feat]  Add PathDistanceToBlockSetter
    막힌 타일까지의 거리를 설정해주는 역할을 할 PathDistanceToBlockSetter 추가

---
+++ Assets/2. Scripts/Pathfind/PathDistanceToBlockSetter.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+
+public class PathDistanceToBlockSetter
+{
+    //▼ 타일의 X와 Y 길이 상수
+    private const int TILE_X_LENGTH = 14;
+    private const int TILE_Y_LENGTH = 8;
+
+    //▼ 해당 방향이 열려있는지 표시하는 상수 bool
+    private const bool OPENED = true;
+    private const bool CLOSED = false;
+
+
+    private DirectionCalculator dirCalc;
+    
+    public void SetBound(PathNodeData[ , ] pathNodeDatas)
+    {
+        int calX = 0;
+        int calY = 0;
+
+        Vector2Int beforeBlock;
+
+
+
+
+
+        
+    }
+}    
+    
