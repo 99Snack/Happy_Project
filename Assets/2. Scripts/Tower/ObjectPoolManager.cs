e57c268c400c2f14e15afbd06e30c860c63200a3
Hrpm <rjsdlek33@gmail.com>
2025-12-23 14:26:49 +0900

    [Feat] Added ObjectPoolManager
    - ObjectPoolManager 에  Pool 기능 추가

---
+++ Assets/2. Scripts/Tower/ObjectPoolManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    public static ObjectPoolManager Instance;
+
+    [System.Serializable]
+    public struct Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Awake()
+    {
+        Instance = this;
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                obj.transform.SetParent(transform);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag) || poolDictionary[tag].Count == 0)
+        {
+            Debug.LogWarning(tag + " 풀이 없거나 비어있습니다.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        objectToSpawn.transform.SetParent(null); // 부모 해제하여 독립적으로 존재
+
+        return objectToSpawn;
+    }
+
+    public void ReturnToPool(string tag, GameObject obj)
+    {
+        if (!poolDictionary.ContainsKey(tag)) return;
+
+        obj.SetActive(false);
+        obj.transform.SetParent(transform); // 다시 매니저 밑으로 정렬
+        poolDictionary[tag].Enqueue(obj);
+    }
+}
