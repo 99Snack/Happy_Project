4562c541801d175c6632090efa1f741110d21ac4
YoungSub <suba1001@daum.net>
2025-12-24 16:38:55 +0900

    [resource] added allData, added font
    - 모든 csv파일 추가 (12개)
    - 모든 폰트 추가 (산체스,프리텐다드)

--- Assets/2. Scripts/Utill/DataManager.cs
+++ Assets/2. Scripts/Utill/DataManager.cs
@@ -24,49 +24,60 @@ public class DataManager : MonoBehaviour
     }
 
     #region Data Table
+    private readonly Dictionary<int, StageData> stageData = new();
+    private readonly Dictionary<int, WaveData> waveData = new();
+    private readonly Dictionary<int, MonsterSpawnGroupData> monsterSpawnGroupData = new();
+    private readonly Dictionary<int, TileData> tileData = new();
+    private readonly Dictionary<int, MonsterData> monsterData = new();
+    private readonly Dictionary<int, RewardData> rewardData = new();
+    private readonly Dictionary<int, RewardGroupData> rewardGroupData = new();
+    private readonly Dictionary<int, TowerBaseData> towerBaseData = new();
+    private readonly Dictionary<int, GachaData> gachaData = new();
+    private readonly Dictionary<int, DebuffData> debuffData = new();
+    private readonly Dictionary<int, AugmentData> augmentData = new();
+    private readonly Dictionary<string, LocalizationData> localizationData = new();
 
-    public Dictionary<int, StageData>                   StageData               = new Dictionary<int, StageData>();
-    public Dictionary<int, WaveData>                    WaveData                = new Dictionary<int, WaveData>();
-    public Dictionary<int, MonsterSpawnGroupData>       MonsterSpawnGroupData   = new Dictionary<int, MonsterSpawnGroupData>();
-    public Dictionary<int, TileData>                    TileData                = new Dictionary<int, TileData>();
-    public Dictionary<int, MonsterData>                 MonsterData             = new Dictionary<int, MonsterData>();
-    public Dictionary<int, RewardData>                  RewardData              = new Dictionary<int, RewardData>();
-    public Dictionary<int, RewardGroupData>             RewardGroupData         = new Dictionary<int, RewardGroupData>();
-    public Dictionary<int, TowerBaseData>               TowerBaseData           = new Dictionary<int, TowerBaseData>();
-    public Dictionary<int, GachaData>                   GachaData               = new Dictionary<int, GachaData>();
-    public Dictionary<int, DebuffData>                  DebuffData              = new Dictionary<int, DebuffData>();
-    public Dictionary<int, AugmentData>                 AugmentData             = new Dictionary<int, AugmentData>();
-    public Dictionary<string, LocalizationData>         LocalizationData        = new Dictionary<string, LocalizationData>();
-
+    public IReadOnlyDictionary<int, StageData> StageData => stageData;
+    public IReadOnlyDictionary<int, WaveData> WaveData => waveData;
+    public IReadOnlyDictionary<int, MonsterSpawnGroupData> MonsterSpawnGroupData => monsterSpawnGroupData;
+    public IReadOnlyDictionary<int, TileData> TileData => tileData;
+    public IReadOnlyDictionary<int, MonsterData> MonsterData => monsterData;
+    public IReadOnlyDictionary<int, RewardData> RewardData => rewardData;
+    public IReadOnlyDictionary<int, RewardGroupData> RewardGroupData => rewardGroupData;
+    public IReadOnlyDictionary<int, TowerBaseData> TowerBaseData => towerBaseData;
+    public IReadOnlyDictionary<int, GachaData> GachaData => gachaData;
+    public IReadOnlyDictionary<int, DebuffData> DebuffData => debuffData;
+    public IReadOnlyDictionary<int, AugmentData> AugmentData => augmentData;
+    public IReadOnlyDictionary<string, LocalizationData> LocalizationData => localizationData;
     #endregion
 
     private void DataParse()
     {
 
         // 스테이지 및 웨이브
-        AddToDictionary(StageData, DataParser.Parse<StageData>("stage"), d => d.Index);
-        AddToDictionary(WaveData, DataParser.Parse<WaveData>("wave"), d => d.Index);
+        AddToDictionary(stageData, DataParser.Parse<StageData>("stage"), d => d.Index);
+        AddToDictionary(waveData, DataParser.Parse<WaveData>("wave"), d => d.Index);
 
         // 몬스터 관련
-        AddToDictionary(MonsterSpawnGroupData, DataParser.Parse<MonsterSpawnGroupData>("mon_spawn_groups"), d => d.Index);
-        AddToDictionary(MonsterData, DataParser.Parse<MonsterData>("monsters"), d => d.MonsterId);
+        AddToDictionary(monsterSpawnGroupData, DataParser.Parse<MonsterSpawnGroupData>("mon_spawn_groups"), d => d.Index);
+        AddToDictionary(monsterData, DataParser.Parse<MonsterData>("monsters"), d => d.MonsterId);
 
         // 타일 및 환경
-        AddToDictionary(TileData, DataParser.Parse<TileData>("tiles"), d => d.Index);
+        AddToDictionary(tileData, DataParser.Parse<TileData>("tiles"), d => d.Index);
 
         // 보상 관련
-        AddToDictionary(RewardData, DataParser.Parse<RewardData>("rewards"), d => d.RewardId);
-        AddToDictionary(RewardGroupData, DataParser.Parse<RewardGroupData>("reward_groups"), d => d.Index);
+        AddToDictionary(rewardData, DataParser.Parse<RewardData>("rewards"), d => d.RewardId);
+        AddToDictionary(rewardGroupData, DataParser.Parse<RewardGroupData>("reward_groups"), d => d.Index);
 
         // 타워 및 전투
-        AddToDictionary(TowerBaseData, DataParser.Parse<TowerBaseData>("tower_base"), d => d.TowerID);
-        AddToDictionary(GachaData, DataParser.Parse<GachaData>("gacha"), d => d.Index);
-        AddToDictionary(DebuffData, DataParser.Parse<DebuffData>("debuff"), d => d.DebuffId);
+        AddToDictionary(towerBaseData, DataParser.Parse<TowerBaseData>("tower_base"), d => d.TowerID);
+        AddToDictionary(gachaData, DataParser.Parse<GachaData>("gacha"), d => d.Index);
+        AddToDictionary(debuffData, DataParser.Parse<DebuffData>("debuff"), d => d.DebuffId);
 
         // 증강
-        AddToDictionary(AugmentData, DataParser.Parse<AugmentData>("augment"), d=> d.Index);
+        AddToDictionary(augmentData, DataParser.Parse<AugmentData>("augment"), d=> d.Index);
 
-        AddToDictionary(LocalizationData, DataParser.Parse<LocalizationData>("localization"), d=> d.Index);
+        AddToDictionary(localizationData, DataParser.Parse<LocalizationData>("localization"), d=> d.Index);
 
         Debug.Log("모든 데이터 파싱 완료");
     }
