b7954490540fb30d86f9c1d3e44d96557fc3f42d
CommonMango <73099231+CommonMango@users.noreply.github.com>
2025-12-26 10:59:51 +0900

    [chore] Delete testscene
    1. PathFindingTest 삭제
    2. testGenerateMonster삭제

--- Assets/2. Scripts/Tile/BaseCamp.cs
+++ Assets/2. Scripts/Tile/BaseCamp.cs
@@ -9,15 +9,14 @@ public class BaseCamp : MonoBehaviour
 
     WaveData waveData;
     public int basecampHp;
-
-    public float ypreset = 0.3f; //test용 public 
-    public float xpreset = 0.3f; //test용 public
+    //▼ 실제로 몬스터가 배치될 위치 값 조정용 오프셋
+    public float xOffset = 0.1f; 
 
     private Queue<int> baseCampOrder = new Queue<int>(); 
 
     [SerializeField] GameObject enemyBaseObj;
     [SerializeField] GameObject allyBaseObj;
-    [SerializeField] GameObject testMonster;
+
 
     private void Awake() 
     {
@@ -29,8 +28,7 @@ public class BaseCamp : MonoBehaviour
         else
         {
             Instance = this;
-        }   
-         
+        }    
     }
 
     //BaseCamp Object 세우기 
@@ -42,17 +40,10 @@ public class BaseCamp : MonoBehaviour
         Vector3 RealEnemyBase = TileManager.Instance.GetWorldPosition(enemyBasePos);
         Vector3 RealAllyBase = TileManager.Instance.GetWorldPosition(allyBasePos);
 
-        RealEnemyBase.y += ypreset;
-        RealAllyBase.y += xpreset;
-
-        RealEnemyBase.x -= xpreset;
-        RealAllyBase.x += xpreset;
-
         Instantiate(enemyBaseObj,RealEnemyBase, Quaternion.identity);
         Instantiate(allyBaseObj,RealAllyBase,Quaternion.identity);
     }
 
-    
     //WaveID 번호를 통해 baseCampHp 가져오기 
     public void SetUp(int waveID)
     {
@@ -75,12 +66,6 @@ public class BaseCamp : MonoBehaviour
         }
     }
 
-    //test: 배치될 위치에 몬스터 생성하는 테스트
-    public void testGenerateMonster()
-    {
-        Instantiate(testMonster, TileManager.Instance.GetWorldPosition(TileManager.Instance.enemyBasePosition),Quaternion.identity);
-    }
-
     //▼ baseCamp 데미지 받기 
     public void TakeDamage(int damage)
     {
@@ -94,7 +79,8 @@ public class BaseCamp : MonoBehaviour
         }
     }
 
-    //▼ Monster가 BaseCamp 내에서 실제로 가아하는 좌표 받기 
+    //▼ Monster가 BaseCamp 내에서 실제로 가아하는 좌표 받기
+
     public Vector3 GetMonsterAttackWorldPos()
     {
         Vector3 destination;
@@ -105,16 +91,16 @@ public class BaseCamp : MonoBehaviour
         baseCampOrder.Enqueue(posOrder);
         
         Vector2Int allyBasePos = TileManager.Instance.allyBasePosition;
-        
         Vector2Int tilePos = new Vector2Int(allyBasePos.x, allyBasePos.y + posOrder); 
         Vector3 tileWorldPos = TileManager.Instance.GetWorldPosition(tilePos); 
 
-        destination = new Vector3(tileWorldPos.x, tileWorldPos.y, tileWorldPos.z + zOffset);
+        destination = new Vector3(tileWorldPos.x - xOffset, tileWorldPos.y, tileWorldPos.z + zOffset);
 
         return destination;
     }
-    
-    //▼ 테스트용 연결 MonsterMove에서 애니메이션 연결해주고GetMonsterAttackWorldPos를 통해 position         받아야함     
+
+    //▼ 테스트용 연결 애니메이션 연결해주고 GetMonsterAttackWorldPos를 통해 position 받아야함
+     
     public void MoveMonsterToAttackPos(GameObject monster)
     {
         Vector3 toPos = GetMonsterAttackWorldPos();
@@ -139,15 +125,6 @@ public class BaseCamp : MonoBehaviour
             monster.transform.position += basedir * Time.deltaTime;
 
             yield return null;   
-        }
-        
-}
-
-
-
-
-
-
-
-
+        } 
+    }
 }
