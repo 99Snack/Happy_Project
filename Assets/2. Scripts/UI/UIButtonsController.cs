0adedde6af01db7f9e9a29fbb893983fbb55b315
Hrpm <rjsdlek33@gmail.com>
2025-12-29 15:40:20 +0900

    [chore] UI 버튼 기능을 UIButtonsController로 통합
    - 게임 종료, 로비씬 이동 버튼, 팝업 열기/닫기, Stage 이동 버튼
      기능을 기존 개별 스크립트에서 UIButtonsController 스크립트 하나로 통일

---
+++ Assets/2. Scripts/UI/UIButtonsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonsController : MonoBehaviour
+{
+    public GameObject popupLayer;
+
+    public void GoToLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void GoToStage()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+
+    public void ShowPopup()
+    {
+        popupLayer.SetActive(true);
+    }
+
+    public void ClosePopup()
+    {
+        popupLayer.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // 에디터 Play 종료
+#else
+        Application.Quit(); // 빌드 게임 종료
+#endif
+    }
+
+}
